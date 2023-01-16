bbc915bfd42cab50c2c31075dcac56f01e9bf7f8
Juancho1952 <juancho1952@gmail.com>
2023-01-16 18:38:39 -0500

    ci: change imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,13 @@
-import { Inter } from '@next/font/google';
-import Head from 'next/head';
-import Layout from '../components/layout';
-import styles from '../styles/Home.module.css';
-
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <div>
       <Layout> </Layout>
-      <Header /> 
+      <Header />
       <Footer />
     </div>
-      
-
-    
   );
 }
