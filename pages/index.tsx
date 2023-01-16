a417042d18620a7a03bbe7f6dbcd51c1f18e396a
Juancho1952 <juancho1952@gmail.com>
2023-01-16 18:18:17 -0500

    Se agrega variables para los colores a utilizar en el archivo tailwind.config.js  junto a las tipografias en el archivo global.css con @layer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head';
 import { Inter } from '@next/font/google';
+import Head from 'next/head';
+import Layout from '../components/layout';
 import styles from '../styles/Home.module.css';
-import Layout from './components/layout';
 
 const inter = Inter({ subsets: ['latin'] });
 
