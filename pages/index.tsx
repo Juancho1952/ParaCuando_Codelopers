e8eea8f056b648130990b99fb7326ef1cd892e80
Juancho1952 <107721234+Juancho1952@users.noreply.github.com>
2023-01-17 20:29:54 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Layout from '../components/Layout';
+import Footer from '../components/footer';
+import Header from '../components/header';
+import Layout from '../components/layout';
 
 export default function Home() {
   return (
